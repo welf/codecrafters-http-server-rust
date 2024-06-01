b10f706cb945b3d7ea2b1a9be5d15472f4665e12
Arthur Welf <arthur.welf@gmail.com>
2024-06-01 07:37:47 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .expect("Failed to write to stream");
             }
             Err(e) => {
                 println!("error: {}", e);
